9b1c792dfd86ad8609eb711ab8234ad077f943a0
Tobias Urdin <tobias.urdin@binero.se>
2019-01-18 10:57:05 +0100

    Remove redundantly tested code
    Remove code that is redundantly tested.
    
    This should not be tested here but in puppet-oslo
    where this logic resides.
    
    If we keep this and we do changes in puppet-oslo we
    will break these unit tests, this is something we need
    to sort out for all modules.
    
    Change-Id: I76e45b6eafcd99bffbfa09cd0d95f915057cb480

--- spec/classes/senlin_db_spec.rb
+++ spec/classes/senlin_db_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'senlin::db' do
-
   shared_examples 'senlin::db' do
     context 'with default parameters' do
-      it { is_expected.to contain_oslo__db('senlin_config').with(
+      it { should contain_class('senlin::deps') }
+
+      it { should contain_oslo__db('senlin_config').with(
         :connection     => 'sqlite:////var/lib/senlin/senlin.sqlite',
         :idle_timeout   => '<SERVICE DEFAULT>',
         :min_pool_size  => '<SERVICE DEFAULT>',
@@ -19,7 +20,8 @@ describe 'senlin::db' do
 
     context 'with specific parameters' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://senlin:senlin@localhost/senlin',
+        {
+          :database_connection     => 'mysql+pymysql://senlin:senlin@localhost/senlin',
           :database_idle_timeout   => '3601',
           :database_min_pool_size  => '2',
           :database_db_max_retries => '-1',
@@ -31,7 +33,9 @@ describe 'senlin::db' do
         }
       end
 
-      it { is_expected.to contain_oslo__db('senlin_config').with(
+      it { should contain_class('senlin::deps') }
+
+      it { should contain_oslo__db('senlin_config').with(
         :connection     => 'mysql+pymysql://senlin:senlin@localhost/senlin',
         :idle_timeout   => '3601',
         :min_pool_size  => '2',
@@ -43,70 +47,6 @@ describe 'senlin::db' do
         :pool_timeout   => '21',
       )}
     end
-
-    context 'with postgresql backend' do
-      let :params do
-        { :database_connection => 'postgresql://senlin:senlin@localhost/senlin', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-psycopg2').with(:ensure => 'present')
-      end
-
-    end
-
-    context 'with MySQL-python library as backend package' do
-      let :params do
-        { :database_connection => 'mysql://senlin:senlin@localhost/senlin', }
-      end
-
-      it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
-    end
-
-    context 'with incorrect database_connection string' do
-      let :params do
-        { :database_connection => 'foodb://senlin:senlin@localhost/senlin', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-    context 'with incorrect pymysql database_connection string' do
-      let :params do
-        { :database_connection => 'foo+pymysql://senlin:senlin@localhost/senlin', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-  end
-
-  shared_examples_for 'senlin::db on Debian' do
-    context 'using pymysql driver' do
-      let :params do
-        { :database_connection => 'mysql+pymysql://senlin:senlin@localhost/senlin', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-pymysql').with(
-          :ensure => 'present',
-          :name   => 'python-pymysql',
-          :tag    => 'openstack'
-        )
-      end
-    end
-  end
-
-  shared_examples_for 'senlin::db on RedHat' do
-    context 'using pymysql driver' do
-      let :params do
-        { :database_connection => 'mysql+pymysql://senlin:senlin@localhost/senlin', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.not_to contain_package('python-pymysql')
-      end
-    end
   end
 
   on_supported_os({
@@ -117,8 +57,7 @@ describe 'senlin::db' do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'senlin::db'
-      it_configures "senlin::db on #{facts[:osfamily]}"
+      it_behaves_like 'senlin::db'
     end
   end
 end
