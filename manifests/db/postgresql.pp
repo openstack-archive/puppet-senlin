77be720c4efbf9612fd3b44b9e98936c15dcf1a6
Takashi Kajinami <tkajinam@redhat.com>
2020-05-20 08:24:19 +0900

    Remove password hash generation in each puppet modules
    ... and migrate it to openstacklib so that all logics about database
    configuration are implemented in one common place.
    
    Depends-on: https://review.opendev.org/#/c/728595/
    Change-Id: I9024b06e50549f2648acc210b4addd9f9629f934

--- manifests/db/postgresql.pp
+++ manifests/db/postgresql.pp
@@ -35,11 +35,11 @@ class senlin::db::postgresql(
   include senlin::deps
 
   ::openstacklib::db::postgresql { 'senlin':
-    password_hash => postgresql_password($user, $password),
-    dbname        => $dbname,
-    user          => $user,
-    encoding      => $encoding,
-    privileges    => $privileges,
+    password   => $password,
+    dbname     => $dbname,
+    user       => $user,
+    encoding   => $encoding,
+    privileges => $privileges,
   }
 
   Anchor['senlin::db::begin']
